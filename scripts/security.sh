1bab617056ff89b2dd4cd5c3a2392b5c9d70bf9c
Andy Botting <andy@andybotting.com>
2014-12-10 15:29:52 +1100

    Remove random root passwd used in testing

--- scripts/security.sh
+++ scripts/security.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -x
 
-# Randomise the root password, then lock
-PASS=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c10)
-echo "Setting root password: $PASS"
-echo "root:$PASS" | chpasswd
-#passwd -l root
-#passwd -d root
+# Randomise the root password
+#PASS=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c10)
+#echo "Setting root password: $PASS"
+#echo "root:$PASS" | chpasswd
+
+# Delete password and lock account
+passwd -d root
+passwd -l root
 
 if [ -f /etc/debian_version ]; then
     sed -i 's/banaction = iptables-multiport/banaction = hostsdeny/' /etc/fail2ban/jail.conf
