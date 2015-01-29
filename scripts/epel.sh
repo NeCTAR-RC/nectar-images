6c3193767b00c271f785a1162ad0c80065f18e56
Andy Botting <andy@andybotting.com>
2015-01-29 13:52:30 +1100

    Use AARNet mirror for all distros

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -6,8 +6,7 @@ RELEASEVER=$(sed -e 's/.*release \([0-9]\+\).*/\1/' /etc/redhat-release)
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-#baseurl=http://mirror.optus.net/epel/${RELEASEVER}/\$basearch
-baseurl=http://mirror.internode.on.net/pub/epel/${RELEASEVER}/\$basearch
+baseurl=http://mirror.aarnet.edu.au/pub/epel/${RELEASEVER}/\$basearch
 enabled=1
 gpgcheck=0
 EOM
