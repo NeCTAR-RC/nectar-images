7a59ea9b9f6a809a4a4be87aa9ae30675cddaab4
Andy Botting <andy@andybotting.com>
2015-02-02 18:30:11 +1100

    Add OpenSuSE 13.2

---
+++ scripts/openstack-suse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+# Move our cloud config into place
+[ -f /tmp/cloud.cfg ] && mv /tmp/cloud.cfg /etc/cloud/cloud.cfg
+
+# Install heat-cfntools
+zypper addrepo -f obs://Cloud:OpenStack:Juno/openSUSE_13.2 Juno
+zypper -n --no-gpg-checks install cloud-init python-heat-cfntools
+
+for SERVICE in cloud-config cloud-final cloud-init cloud-init-local; do
+    systemctl enable $SERVICE
+done
+
+# Common kernel args
+KERNEL_ARGS=" splash=slient showopts elevator=noop console=ttyS0,115200n8 console=tty0 consoleblank=0"
+
+# change GRUB so log tab and console tab in openstack work
+if [ -e /etc/default/grub ] ; then
+    sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT=\" ${KERNEL_ARGS}\"/g" /etc/default/grub
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+fi
+
+# Make sure sudo works properly with openstack
+sed -i "s/^.*requiretty$/Defaults !requiretty/" /etc/sudoers
