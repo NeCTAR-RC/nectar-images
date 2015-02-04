dd806ad74f2a808e25e639ff81aeaf0677c013f7
Andy Botting <andy@andybotting.com>
2015-02-04 16:47:55 +1100

    Re-work fail2ban setup for all distros

---
+++ scripts/fail2ban.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+set -x
+
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+elif [ -f /etc/redhat-release ]; then
+    NAME=$(sed -e 's/\(.*\)release\? \([0-9]\+\).*/\1/' /etc/redhat-release)
+    ID=$(echo $NAME | tr '[:upper:]' '[:lower:]')
+    VERSION_ID=$(sed -e 's/.*release\? \([0-9]\+\).*/\1/' /etc/redhat-release)
+else
+    NAME=Unknown
+    ID=Unknown
+    VERSION_ID=Unknown
+fi
+
+#
+# RedHat based distros
+#
+if [ "$ID" == "centos" ] || [ "$ID" == "fedora" ]; then
+    yum -q -yy install fail2ban
+
+    # Make our jail.d directory if doesn't exist
+    [ -d /etc/fail2ban/jail.d ] || mkdir /etc/fail2ban/jail.d
+
+    # On newer distros, remove the firewalld package and the fail2ban
+    # empty meta-package too. We'll provide our own config
+    if rpm -q fail2ban-firewalld &>/dev/null; then 
+        rpm -e fail2ban fail2ban-firewalld
+    fi
+
+    # CentOS 7, Fedora 20+
+    if [ $VERSION_ID -eq 7 ] || [ $VERSION_ID -ge 20 ]; then
+        cat > /etc/fail2ban/jail.d/00-hostdeny.conf << EOF
+# NeCTAR: use hostsdeny by default
+[DEFAULT]
+banaction = hostsdeny
+EOF
+
+        cat > /etc/fail2ban/jail.d/01-sshd.conf << EOF
+# NeCTAR: enable SSH jail
+[sshd]
+enabled = true
+EOF
+
+    systemctl enable fail2ban
+
+    # CentOS 6
+    elif [ $VERSION_ID -eq 6 ]; then
+        cat > /etc/fail2ban/jail.d/01-ssh-iptables.conf << EOF
+# NeCTAR doesn't use iptables by default
+[ssh-iptables]
+enabled = false
+EOF
+
+        cat > /etc/fail2ban/jail.d/02-ssh-tcpwrapper.conf << EOF
+# NeCTAR: use tcpwrapper SSH
+[ssh-tcpwrapper]
+enabled     = true
+filter      = sshd
+action      = hostsdeny[daemon_list=sshd]
+logpath     = /var/log/secure
+EOF
+    fi
+
+    chkconfig --add fail2ban
+
+#
+# Debian 7
+#
+elif [ "$ID" == "debian" ]; then
+
+    # Install fail2ban package
+    apt-get -qq -y install fail2ban
+
+    # Debian 7 doesn't support jail.d
+    cat > /etc/fail2ban/jail.local << EOF
+[DEFAULT]
+banaction = hostsdeny
+
+[ssh]
+enabled  = true
+port     = ssh
+filter   = sshd
+logpath  = /var/log/auth.log
+maxretry = 6
+EOF
+
+#
+# Ubuntu 14.04+
+#
+elif [ "$ID" == "ubuntu" ]; then
+
+    # Install fail2ban package
+    apt-get -qq -y install fail2ban
+
+    cat > /etc/fail2ban/jail.d/00-hostsdeny.conf << EOF
+# NeCTAR: use hostsdeny by default
+[DEFAULT]
+banaction = hostsdeny
+EOF
+
+    cat > /etc/fail2ban/jail.d/01-ssh.conf << EOF
+# NeCTAR: enable SSH jail
+[ssh]
+enabled = true
+EOF
+
+#
+# OpenSuSE 13.2
+#
+elif [ "$ID" == "opensuse" ]; then
+    # Install fail2ban package
+    zypper -n --no-gpg-checks install fail2ban
+    systemctl enable fail2ban
+    
+    # Make our jail.d directory if doesn't exist
+    [ -d /etc/fail2ban/jail.d ] || mkdir /etc/fail2ban/jail.d
+
+    cat > /etc/fail2ban/jail.d/01-ssh-iptables.conf << EOF
+# NeCTAR doesn't use iptables by default
+[ssh-iptables]
+enabled = false
+EOF
+
+    cat > /etc/fail2ban/jail.d/02-ssh-tcpwrapper.conf << EOF
+# NeCTAR: use tcpwrapper SSH
+[ssh-tcpwrapper]
+enabled     = true
+EOF
+fi
+
+
+# Some new distros don't ship the hostsdeny action
+if [ ! -e /etc/fail2ban/action.d/hostsdeny.conf ]; then
+    cat > /etc/fail2ban/action.d/hostsdeny.conf << EOF
+[Definition]
+actionstart = 
+actionstop = 
+actioncheck = 
+actionban = IP=<ip> &&
+            printf %%b "<daemon_list>: $IP\n" >> <file>
+actionunban = echo "/^<daemon_list>: <ip>$/<br>d<br>w<br>q" | ed <file>
+
+[Init]
+file = /etc/hosts.deny
+daemon_list = ALL
+EOF
+fi
--- scripts/security.sh
+++ scripts/security.sh
@@ -5,11 +5,6 @@ set -x
 passwd -d root
 passwd -l root
 
-if [ -f /etc/debian_version ]; then
-    apt-get -qq -y install fail2ban
-    sed -i 's/banaction = iptables-multiport/banaction = hostsdeny/' /etc/fail2ban/jail.conf
-fi
-
 # Disable password auth for SSH
 sed -i -e 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i -e 's/^#\?PermitRootLogin.*/PermitRootLogin without-password/g' /etc/ssh/sshd_config
