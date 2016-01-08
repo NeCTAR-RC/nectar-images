4f6257d9ffb5302ffb515a9b16247f0159fa5410
Andy Botting <andy@andybotting.com>
2016-01-08 18:39:45 +1100

    Remove hivemind install from jenkins buildslave for now
    Change-Id: Id9b8447ebb5f213a9b59a32d13e522b417393514

--- scripts/jenkins-slave.sh
+++ scripts/jenkins-slave.sh
@@ -7,7 +7,6 @@ add-apt-repository cloud-archive:liberty
 apt-get -qq -y update
 apt-get -qq -y install openjdk-7-jdk rake ruby-puppetlabs-spec-helper puppet-lint git bundler python-dev libssl-dev libxml2-dev libxslt-dev python-tox python-pip build-essential libmysqlclient-dev libfreetype6-dev libpng12-dev git-buildpackage debhelper dupload libffi-dev npm nodejs libpq-dev unzip qemu pkg-config libvirt-dev libsqlite3-dev libldap2-dev libsasl2-dev
 apt-get -qq -y install dh-systemd openstack-pkg-tools python-sphinx python3-setuptools libcurl3 python-ceilometerclient python-cinderclient python-designateclient python-glanceclient python-heatclient python-keystoneclient python-muranoclient python-neutronclient python-novaclient python-openstackclient python-swiftclient
-apt-get -qq -y install python-hivemind python-hivemind-contrib
 
 apt-get -qq -y autoremove
 apt-get -qq -y autoclean
