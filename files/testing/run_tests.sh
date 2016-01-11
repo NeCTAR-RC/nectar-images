6a8f659c70e35c33eaf87a78d56a7e2b75923d7a
Andy Botting <andy@andybotting.com>
2016-01-11 11:23:35 +1100

    Add basic testing to images
    Change-Id: I671e844a4927ef795b9d0145c69541bd8f78ce06

---
+++ files/testing/run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Source testing framework
+source $(dirname $0)/assert.sh
+
+### Ephemeral disk checking 
+assert_raises "grep -q '/dev/vdb.*ext4.*rw' /proc/mounts"
+assert_end "Check that ephemeral disk is ext4 and read-write mounted on vdb"
+
+### Fail2ban
+assert_raises "pgrep fail2ban"
+assert_end "Fail2ban is running"
+
+### heat-cfntools
+assert_raises "which cfn-create-aws-symlinks cfn-get-metadata cfn-push-stats cfn-hup cfn-init cfn-signal"
+assert_end "heat-cfntools are installed"
+
+### No default passwords
+assert_raises "test \"$(sudo cut -d ':' -f 2 /etc/shadow | cut -d '$' -sf3)\" = ''"
+assert_end "No default passwords exist"
+
+### NTP running
+assert_raises "pgrep ntp"
+assert_end "NTP service is running"
+
+### SSH key for root
+assert "sudo wc -l /root/.ssh/authorized_keys | cut -d ' ' -f1" 1
+assert_end "Single SSH authorized key for root exists"
+
+### SSH key for current user
+assert "wc -l ~/.ssh/authorized_keys | cut -d ' ' -f1" 1
+assert_end "Single SSH authorized key for current user exists"
