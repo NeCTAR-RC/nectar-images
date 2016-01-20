f166371ed67dd57179be9c19424e9bc5bed2cf90
Andy Botting <andy@andybotting.com>
2016-01-20 14:27:12 +1100

    Change ordering of package tests to make opensuse work
    Seems our opensuse 42 has apt-get installed
    
    Change-Id: If975197430ff0d4ffaa65b4c5d492f54404fa4e5

--- files/testing/run_tests.sh
+++ files/testing/run_tests.sh
@@ -42,7 +42,11 @@ assert "wc -l ~/.ssh/authorized_keys | cut -d ' ' -f1" 1
 assert_end "Single SSH authorized key for current user exists"
 
 ### Package repos
-if hash dnf 2>/dev/null; then
+if hash zypper 2>/dev/null; then
+    assert_raises "sudo zypper --gpg-auto-import-keys refresh"
+    assert_raises "sudo zypper clean"
+    assert_end "Package repositories are OK"
+elif hash dnf 2>/dev/null; then
     assert_raises "sudo dnf makecache"
     assert_raises "sudo dnf clean all"
     assert_end "Package repositories are OK"
@@ -53,10 +57,6 @@ elif hash yum 2>/dev/null; then
 elif hash apt-get 2>/dev/null; then
     assert_raises "sudo apt-get update"
     assert_raises "sudo apt-get -y clean all"
-    assert_end "Package repositories are OK"
-elif hash zypper 2>/dev/null; then
-    assert_raises "sudo zypper --gpg-auto-import-keys refresh"
-    assert_raises "sudo zypper clean"
     assert_end "Package repositories are OK"
 fi
 
