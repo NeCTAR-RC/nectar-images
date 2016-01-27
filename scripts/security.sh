db6e4b5603d70b7bcf7c1bb9e9bec3ef73420aa4
Marcus Furlong <furlongm@gmail.com>
2016-01-28 10:46:17 +1100

    ensure all security updates are installed
    Change-Id: I14145565d0088a79cb40cea2354e122e8a802763

--- scripts/security.sh
+++ scripts/security.sh
@@ -9,3 +9,16 @@ passwd -l root
 # Disable password auth for SSH
 sed -i -e 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i -e 's/^#\?PermitRootLogin.*/PermitRootLogin without-password/g' /etc/ssh/sshd_config
+
+# Ensure all installed packages are up-to-date
+if [ -f '/etc/debian_version' ] ; then
+    apt-get -qq -y update
+    apt-get -y dist-upgrade
+elif [ -f '/etc/redhat-release' ] ; then
+    yum -y clean all
+    yum -q -y makecache
+    yum -y upgrade
+elif [ -f '/etc/SuSE-release' ] ; then
+    zypper -q -n ref
+    zypper -n dup
+fi
