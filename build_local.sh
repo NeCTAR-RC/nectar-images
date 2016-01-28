129477b6330d9b518328bb0f29422ad8ffe9c5c2
Marcus Furlong <furlongm@gmail.com>
2016-01-28 16:31:02 +1100

    various minor formatting fixes
    Change-Id: Ib2ebaa9e57f270aa2342187059de396777aedf4d

--- build_local.sh
+++ build_local.sh
@@ -6,8 +6,8 @@
 #  - Jenkins image testing SSH key in your ssh-agent
 
 FILE=$1
-if [ -z $FILE ]; then
-    echo "Give file to build"
+if [ -z ${FILE} ]; then
+    echo "Usage: $0 [JSONFILE]"
     exit 1
 fi
 
@@ -22,43 +22,42 @@ if which packer >/dev/null 2>&1; then
 elif which packer-io >/dev/null 2>&1; then
     PACKER=packer-io
 else
-    echo "You need packer installed to use this"
+    echo "You need packer installed to use this tool"
     exit 1
 fi
 
-NAME=$(basename -s .json $FILE)
+NAME=$(basename -s .json ${FILE})
 BUILD_NUMBER=$(date "+%Y%m%d%H%M")
+OUTPUT_DIR=output-${NAME}
 
-if [ -d output-$NAME ]; then
-    echo "Cleaning up exising output directory \"output-$NAME \"..."
-    rm -fr output-$NAME
+if [ -d ${OUTPUT_DIR} ]; then
+    echo "Cleaning up existing output directory \"${OUTPUT_DIR}\"..."
+    rm -fr ${OUTPUT_DIR}
 fi
 
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+if [ -n "${SSH_CLIENT}" ] || [ -n "${SSH_TTY}" ]; then
     echo "Running over SSH shell, not enabling headless mode"
-    PACKER_FILE=$FILE
+    PACKER_FILE=${FILE}
 else
     # For local builds, we'll disable headless mode
-    sed 's/"headless": true/"headless": false/g' $NAME.json >/tmp/temp-$NAME.json
-    PACKER_FILE=/tmp/temp-$NAME.json
+    sed 's/"headless": true/"headless": false/g' ${NAME}.json >/tmp/temp-${NAME}.json
+    PACKER_FILE=/tmp/temp-${NAME}.json
 fi
 
-echo "Building image $NAME..."
-$PACKER build $PACKER_FILE
-[ -f /tmp/temp-$NAME.json ] && rm /tmp/temp-$NAME.json
+echo "Building image ${NAME}..."
+${PACKER} build -var-file=${PACKER_VARS} ${PACKER_FILE}
+[ -f /tmp/temp-${NAME}.json ] && rm /tmp/temp-${NAME}.json
 
 echo "Shrinking image..."
-cd output-$NAME
-qemu-img convert -c -o compat=0.10 -O qcow2 $NAME $NAME.qcow2
-rm $NAME
-cd ..
+qemu-img convert -c -o compat=0.10 -O qcow2 ${OUTPUT_DIR}/${NAME} ${OUTPUT_DIR}/${NAME}.qcow2
+rm ${OUTPUT_DIR}/${NAME}
 
-echo "Creating image \"NeCTAR $NAME\"..."
-IMAGE_ID=$(openstack image create --os-image-api-version=1 -f value -c id --disk-format qcow2 --container-format bare --file output-$NAME/$NAME.qcow2 --property nectar_build=$BUILD_NUMBER "NeCTAR $NAME")
-echo "Image ID: $IMAGE_ID"
+echo "Creating image \"${IMAGE_NAME}\"..."
+IMAGE_ID=$(openstack image create --os-image-api-version=1 -f value -c id --disk-format qcow2 --container-format bare --file ${OUTPUT_DIR}/${NAME}.qcow2 --property nectar_build=${BUILD_NUMBER} "NeCTAR ${NAME}")
+echo "Image ID: ${IMAGE_ID}"
 
 echo "Removing image working directory..."
-rm -fr rm -fr output-$NAME
+rm -fr ${OUTPUT_DIR}
 
 echo "Creating instance \"test_${NAME}_$BUILD_NUMBER\"..."
 INSTANCE_ID=$(openstack server create -f value -c id --image $IMAGE_ID --flavor m1.small --key-name jenkins-image-testing "test_${NAME}_$BUILD_NUMBER")
@@ -69,7 +68,7 @@ set +e
 ATTEMPTS=50
 ATTEMPT=1
 while [ $ATTEMPT -le $ATTEMPTS ]; do
-    STATUS=$(openstack server show -f value -c status $INSTANCE_ID)
+    STATUS=$(openstack server show -f value -c status ${INSTANCE_ID})
     [ "$STATUS" = "ACTIVE" ] && break
     echo "Waiting for instance to become ACTIVE [$STATUS]... ($ATTEMPT/$ATTEMPTS)"
     [ $ATTEMPT -eq $ATTEMPTS ] && exit 1
@@ -77,13 +76,13 @@ while [ $ATTEMPT -le $ATTEMPTS ]; do
     sleep 5
 done
 
-IP_ADDRESS=$(openstack server show -f value -c accessIPv4 $INSTANCE_ID)
+IP_ADDRESS=$(openstack server show -f value -c accessIPv4 ${INSTANCE_ID})
 echo "Instance has IP address: $IP_ADDRESS"
 
 # Discover which user account to use
-if echo "$NAME" | grep -qi debian; then
+if echo "${NAME}" | grep -qi debian; then
     USER_ACCOUNT=debian
-elif echo "$NAME" | grep -q ubuntu; then
+elif echo "${NAME}" | grep -q ubuntu; then
     USER_ACCOUNT=ubuntu
 else
     USER_ACCOUNT=ec2-user
@@ -102,5 +101,5 @@ done
 echo "Running tests (ssh $USER_ACCOUNT@$IP_ADDRESS '/bin/bash /var/lib/packer/build/run_tests.sh')..."
 ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null $USER_ACCOUNT@$IP_ADDRESS '/bin/bash /var/lib/packer/build/run_tests.sh'
 
-echo "Deleting instance $INSTANCE_ID..."
-openstack server delete $INSTANCE_ID
+echo "Deleting instance ${INSTANCE_ID}..."
+openstack server delete ${INSTANCE_ID}
