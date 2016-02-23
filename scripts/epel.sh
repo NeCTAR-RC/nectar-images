5e982641bae966882f8144b0b4887b58ec06fa65
Marcus Furlong <furlongm@gmail.com>
2016-02-23 14:58:53 +1100

    use upstream epel url
    Change-Id: I6aa3d5f61476a688c27faff10d0bdb31ad1f2f02

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -7,7 +7,7 @@ RELEASEVER=$(sed -e 's/.*release \([0-9]\+\).*/\1/' /etc/redhat-release)
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://mirror.aarnet.edu.au/pub/epel/${RELEASEVER}/\$basearch
+baseurl=http://dl.fedoraproject.org/pub/epel/${RELEASEVER}/\$basearch
 enabled=1
 gpgcheck=0
 EOM
