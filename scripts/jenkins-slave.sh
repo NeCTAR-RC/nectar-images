f24f910e60d7265427d467bcde8ecb04233adcb3
Andy Botting <andy@andybotting.com>
2016-03-04 13:25:17 +1100

    More verbose output when installing packages and npm is now part of nodejs
    Change-Id: I3ad32a949601950d9b475185d791b72ef5639fab

--- scripts/jenkins-slave.sh
+++ scripts/jenkins-slave.sh
@@ -8,13 +8,14 @@ add-apt-repository cloud-archive:liberty
 apt-key adv --keyserver keyserver.ubuntu.com --recv 68576280
 apt-add-repository "deb https://deb.nodesource.com/node_4.x $(lsb_release -sc) main"
 
-apt-get -qq -y update
-apt-get -qq -y install openjdk-7-jdk rake ruby-puppetlabs-spec-helper puppet-lint git bundler python-dev libssl-dev libxml2-dev libxslt-dev build-essential libmysqlclient-dev libfreetype6-dev libpng12-dev git-buildpackage debhelper dupload libffi-dev npm nodejs libkrb5-dev libpq-dev unzip qemu pkg-config libvirt-dev libsqlite3-dev libldap2-dev libsasl2-dev python-pip
+apt-get -q -y update
+apt-get -q -y install openjdk-7-jdk rake ruby-puppetlabs-spec-helper puppet-lint git bundler python-dev libssl-dev libxml2-dev libxslt-dev build-essential libmysqlclient-dev libfreetype6-dev libpng12-dev git-buildpackage debhelper dupload libffi-dev npm nodejs libkrb5-dev libpq-dev unzip qemu pkg-config libvirt-dev libsqlite3-dev libldap2-dev libsasl2-dev python-pip
 apt-get -qq -y install dh-systemd openstack-pkg-tools python-sphinx python3-setuptools libcurl3 python-ceilometerclient python-cinderclient python-designateclient python-glanceclient python-heatclient python-keystoneclient python-muranoclient python-neutronclient python-novaclient python-openstackclient python-swiftclient
+apt-get -q -y install nodejs libkrb5-dev
 
-apt-get -qq -y autoremove
-apt-get -qq -y autoclean
-apt-get -qq -y clean
+apt-get -q -y autoremove
+apt-get -q -y autoclean
+apt-get -q -y clean
 
 # Needed to build Liberty on Trusty
 pip install --upgrade 'pip==7.1.2' 'virtualenv==13.1.2' 'setuptools==19.4' 'pbr==1.8.1' 'tox==2.3.1'
