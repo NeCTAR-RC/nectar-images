368a839d02873b8d2d9c2f47f70d95e3d60eb617
Andy Botting <andy@andybotting.com>
2016-03-16 08:40:46 +1100

    Add Murano agent to Ubuntu 14.04
    Change-Id: I134441f66799083dc8f16d00c8c0306812ac95c7

---
+++ scripts/murano-agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+
+# Test OS
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+fi
+
+# We only support murano agent on Ubuntu 14.04 for now
+if [ "$ID" == "ubuntu" ] && [ "$VERSION_ID" == "14.04" ]; then
+	# We require Mitaka cloud archive for deps
+	add-apt-repository -y cloud-archive:mitaka
+	# Package only available for Wiley
+	wget http://au.archive.ubuntu.com/ubuntu/pool/universe/m/murano-agent/murano-agent_1.0.0~b3-1_all.deb -O /tmp/murano-agent_1.0.0~b3-1_all.deb
+	dpkg -i /tmp/murano-agent_1.0.0~b3-1_all.deb || true # keep script running
+	apt-get -qq -y install -f
+	rm -f /tmp/murano-agent_1.0.0~b3-1_all.deb
+else
+    # Not supported
+	exit 1
+fi
