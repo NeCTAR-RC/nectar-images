75025362312fe27173d0d4a4229eb5a5cd5ec9e4
Marcus Furlong <furlongm@gmail.com>
2016-09-22 12:40:57 +1000

    support for variable orgname and support url
    Change-Id: I057dd0bee654fdf21afba7724d93ed82bc8f2ffd

--- scripts/banner.sh
+++ scripts/banner.sh
@@ -2,6 +2,14 @@
 set -x
 set -e
 
+if [ -z "${ORGANISATION}" ] ; then
+    ORGANISATION="NeCTAR"
+fi
+
+if [ -z "${IMAGE_SUPPORT_URL}" ] ; then
+    IMAGE_SUPPORT_URL="https://support.nectar.org.au/support/solutions/articles/6000106269-image-catalog"
+fi
+
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     OSSTRING=$PRETTY_NAME
@@ -25,18 +33,18 @@ if [ -z "\$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
 fi
 
 echo "-------------------------------------------------------------------------------------"
-echo "  NeCTAR \$DISTRIB_DESCRIPTION \$(uname -i)"
+echo "  ${ORGANISATION} \$DISTRIB_DESCRIPTION \$(uname -i)"
 echo "  Image details and information is available at"
-echo "  https://support.nectar.org.au/support/solutions/articles/6000106269-image-catalog"
+echo "  ${IMAGE_SUPPORT_URL}"
 echo "-------------------------------------------------------------------------------------"
 EOF
 else
     # Add motd
     cat > /etc/motd << EOF
 -------------------------------------------------------------------------------------
-  NeCTAR $OSSTRING
+  ${ORGANISATION} ${OSSTRING}
   Image details and information is available at
-  https://support.nectar.org.au/support/solutions/articles/6000106269-image-catalog
+  ${IMAGE_SUPPORT_URL}
 -------------------------------------------------------------------------------------
 
 EOF
