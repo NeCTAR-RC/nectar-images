1fe5dc3ceb0c482040271cdeba00a02dafbb05ad
Andy Botting <andy@andybotting.com>
2017-03-17 09:19:50 +1100

    Switch to Ansible and OpenStack builder
    Change-Id: Ifc696be78a2d9f7ad2dba649e7ba1ca1379b54ec

---
+++ ansible/roles/testing/files/testing/run_tests.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Source testing framework
+source $(dirname $0)/assert.sh
+
+echo ""
+echo "================================================================================="
+echo "                                 Running Tests                                   "
+echo "================================================================================="
+echo ""
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+elif [ -f /etc/redhat-release ]; then
+    NAME=$(sed -e 's/\(.*\)release\? \([0-9]\+\).*/\1/' /etc/redhat-release)
+    ID=$(echo $NAME | tr '[:upper:]' '[:lower:]')
+    VERSION_ID=$(sed -e 's/.*release\? \([0-9]\+\).*/\1/' /etc/redhat-release)
+else
+    echo "Disto ID check fail"
+    exit 1
+fi
+
+### Ensure OpenStack datasource is used by cloud-init
+assert_raises "tail -n1 /var/log/cloud-init-output.log | grep 'DataSourceOpenStack'"
+assert_end "Check that OpenStack data source is used by cloud-init"
+
+### I/O scheduler is none/noop
+assert "grep -Ev '(none|\[noop\])' /sys/block/*/queue/scheduler" ''
+assert_end "Check that none/noop I/O scheduler in use"
+
+### Ephemeral disk checking
+skip_if "test ! -b /dev/vdb" # skip is doesn't exist
+assert_raises "grep '/dev/vdb.*ext4.*rw' /proc/mounts"
+assert_end "Check that ephemeral disk is ext4 and read-write mounted on vdb"
+
+### Root disk resized
+assert "test $(df -P -BG / | sed -n -e 's/^\/\s\+\([0-9]\+\)G.*/\1/p') -gt 4"
+assert_end "Root filesystem resized"
+
+### Serial console set in right order
+assert_raises "grep -E 'console=tty0\s.*console=ttyS0' /proc/cmdline"
+assert_end "Kernel console log configured correctly"
+
+### Default interface is eth0
+assert "/sbin/ip route | grep -E 'default via .* dev eth0'"
+assert_end "Default route via interface named eth0"
+
+### Fail2ban
+assert_raises "pgrep fail2ban"
+assert_end "Fail2ban is running"
+
+### heat-cfntools for everything except CentOS 5
+skip_if "test \"$ID\" = \"centos\" -a $VERSION_ID -lt 6"
+assert_raises "which cfn-create-aws-symlinks cfn-get-metadata cfn-push-stats cfn-hup cfn-init cfn-signal"
+assert_end "heat-cfntools are installed"
+
+### No default passwords
+assert_raises "test \"$(sudo cut -d ':' -f 2 /etc/shadow | cut -d '$' -sf3)\" = ''"
+assert_end "No default passwords exist"
+
+### NTP running
+assert_raises "pgrep 'ntp|chronyd'"
+assert_end "NTP or chrony service is running"
+
+### SSH key for root
+assert "sudo wc -l /root/.ssh/authorized_keys | cut -d ' ' -f1" 1
+assert_end "Single SSH authorized key for root exists"
+
+### SSH key for current user
+assert "wc -l ~/.ssh/authorized_keys | cut -d ' ' -f1" 1
+assert_end "Single SSH authorized key for current user exists"
+
+### Only one user home directory
+assert "test $(find /home -mindepth 1 -maxdepth 1 -type d) -eq 1"
+assert_end "Single home directory exists in /home"
+
+### Package repos
+if hash zypper 2>/dev/null; then
+    assert_raises "sudo zypper --gpg-auto-import-keys refresh"
+    assert_raises "sudo zypper clean"
+    assert_end "Package repositories are OK"
+elif hash dnf 2>/dev/null; then
+    assert_raises "sudo dnf makecache"
+    assert_raises "sudo dnf clean all"
+    assert_end "Package repositories are OK"
+elif hash yum 2>/dev/null; then
+    assert_raises "sudo yum makecache"
+    assert_raises "sudo yum clean all"
+    assert_end "Package repositories are OK"
+elif hash apt-get 2>/dev/null; then
+    assert_raises "sudo apt-get update"
+    assert_raises "sudo apt-get -y clean all"
+    assert_end "Package repositories are OK"
+fi
+
+echo ""
+echo "================================================================================="
+echo "                                Tests Complete                                   "
+echo "================================================================================="
+echo ""
