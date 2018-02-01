5725fb6b9687a950da1ca46082f736ccc5e56b6d
Andy Botting <andy@andybotting.com>
2018-02-02 10:25:08 +1100

    Add QEMU guest agent
    Change-Id: I681b9676c5af1a2eb09c5b1e6efc009c9530bc25

---
+++ ansible/roles/qemu-guest-agent/files/100-qemu-running.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. $(dirname $0)/../assert.sh
+
+assert_raises "pgrep qemu-ga"
+assert_end "QEMU guest agent is running"
+
+assert_raises "ps --no-heading -o cmd -p $(pgrep qemu-ga) | grep -E 'blacklist.guest'"
+assert_end "QEMU is started with blacklist"
