f15d95ad6abcb5a401e63b95bc46cf3cc3bbbd0a
Andy Botting <andy@andybotting.com>
2018-02-21 08:56:14 +1100

    Fix test return code
    Change-Id: I4e5b68fde44bd53737a956130198aa10a54f6c65

--- ansible/roles/testing/files/testing/run_tests.sh
+++ ansible/roles/testing/files/testing/run_tests.sh
@@ -15,12 +15,18 @@ echo ""
 for file in "$TESTS_D"/*.sh ; do
     [ -x "$file" ] || continue
     "$file" "$@"
-    [ $? -gt 0 ] && RET=$STATUS
+    [ $? -gt 0 ] && RET=255
 done
+
+if [ $RET -eq 0 ]; then
+    STATUS=Success
+else
+    STATUS=Failure
+fi
 
 echo ""
 echo "================================================================================="
-echo "                                Tests Complete                                   "
+echo "                                 Tests $STATUS                                   "
 echo "================================================================================="
 echo ""
 
