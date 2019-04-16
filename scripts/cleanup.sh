3870310f4f438023fa43210d7dc89b581c9ef4dc
Andy Botting <andy@andybotting.com>
2019-04-16 15:25:50 +1000

    Truncate log files right at the end
    Change-Id: I468876d8fcf413f61e7783e91b683464ba4d3f36

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,6 +5,9 @@ rm -fr /home/*/{.ssh,.ansible,.cache}
 rm -fr /root/{.ssh,.ansible,.cache}
 rm -fr /root/'~'*
 
+# Truncate any log files
+find /var/log -type f -print0 | xargs -0 truncate -s0
+
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 sync
