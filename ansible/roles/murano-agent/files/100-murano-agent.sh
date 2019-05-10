587a550e95047cc22461d3d74da5d3b5f3d7172b
Andy Botting <andy@andybotting.com>
2019-05-10 13:28:25 +1000

    Move kernel cmd line args to kickstart/preseed
    Logic is getting too complicated in Ansible for this, so move it
    completely to the kickstart/preseed stage. This will fix an issue
    with Fedora 30
    
    Also make some changes to the qemu guest agent script to better
    support newer distros that lack an environment file for config
    
    Change-Id: I4d713a3b2a5776e75160ad13a08c88491d913bdc

--- ansible/roles/murano-agent/files/100-murano-agent.sh
+++ ansible/roles/murano-agent/files/100-murano-agent.sh
@@ -2,5 +2,5 @@
 . $(dirname $0)/../assert.sh
 
 ### murano-agent service is installed and not running (return code 3)
-assert_raises "/sbin/service murano-agent status" 3
+assert_raises "service murano-agent status" 3
 assert_end "murano-agent is installed and disabled"
