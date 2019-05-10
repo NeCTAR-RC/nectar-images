0c0a166934b5461ebe23660aec35ea85019b9cdb
Andy Botting <andy@andybotting.com>
2019-05-10 13:28:25 +1000

    Move kernel cmd line args to kickstart/preseed
    Logic is getting too complicated in Ansible for this, so move it
    completely to the kickstart/preseed stage. This will fix an issue
    with Fedora 30
    
    Change-Id: I4d713a3b2a5776e75160ad13a08c88491d913bdc

--- ansible/roles/qemu-guest-agent/files/100-qemu-running.sh
+++ ansible/roles/qemu-guest-agent/files/100-qemu-running.sh
@@ -3,6 +3,3 @@
 
 assert_raises "pgrep qemu-ga"
 assert_end "QEMU guest agent is running"
-
-assert_raises "ps --no-heading -o cmd -p $(pgrep qemu-ga) | grep -E 'blacklist.guest'"
-assert_end "QEMU guest agent started with blacklist"
