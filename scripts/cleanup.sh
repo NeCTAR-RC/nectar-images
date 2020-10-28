21ba26f03ee102605f1e2faf630b739f8a5fb613
Andy Botting <andy@andybotting.com>
2020-10-28 18:41:45 +1100

    Cleanups for Ubuntu
    Change-Id: Ia7463a009f5756cbb188ad97f9e3f0904cd2838c

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,6 +5,12 @@ rm -fr /home/*/{.ssh,.ansible,.cache}
 rm -fr /root/{.ssh,.ansible,.cache}
 rm -fr /root/'~'*
 
+# Clean up any default users
+userdel -rf ec2-user || true
+userdel -rf debian || true
+userdel -rf fedora || true
+userdel -rf ubuntu || true
+
 # Truncate any log files
 find /var/log -type f -print0 | xargs -0 truncate -s0
 
