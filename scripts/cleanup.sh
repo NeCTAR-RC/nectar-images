fcd04ae4eb3b330ada45bfc5b87863320f3d83c1
Dylan McCulloch <dylan.mcculloch@gmail.com>
2021-12-03 08:35:41 +1100

    Ensure resolv.conf is empty file if it exists in image
    Resolves CentOS bug [1], but fix is included in generic cleanup
    script to ensure file is empty in all images to allow cloud-init
    to configure DNS.
    
    [1] https://bugs.centos.org/view.php?id=14369
    
    Change-Id: Ibbe55a52f622bfb1ab248397d95cda5de4a772fc

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -15,6 +15,10 @@ userdel -rf rocky || true
 # Truncate any log files
 find /var/log -type f -print0 | xargs -0 truncate -s0
 
+# Truncate resolv.conf if it exists
+# https://bugs.centos.org/view.php?id=14369
+truncate -c -s 0 /etc/resolv.conf
+
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 sync
